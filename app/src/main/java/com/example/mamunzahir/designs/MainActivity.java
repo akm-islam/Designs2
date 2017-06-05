b3cde114385f4eb3cadb9466147d65656faee6d9
Mamunjahir <mamunjahir.com@gmail.com>
2017-06-05 01:25:06 -0400

    Final

--- app/src/main/java/com/example/mamunzahir/designs/MainActivity.java
+++ app/src/main/java/com/example/mamunzahir/designs/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.myfinal);
 
         // Get the application context
         mContext = getApplicationContext();
