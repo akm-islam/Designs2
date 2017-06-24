10dc94538649d34a3e8a216a6391c432dd0d31f1
Mamunjahir <mamunjahir.com@gmail.com>
2017-06-23 23:07:00 -0400

    WeatherApp Final

---
+++ app/src/main/java/com/example/mamunzahir/designs/AppController.java
@@ -0,0 +1,70 @@
+package com.example.mamunzahir.designs;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by paulodichone on 3/14/15.
+ */
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        mInstance = this;
+        // when appcontroller is created minstance is set as an appcontroller null object (may be the oncreate is creating an object) ;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // very expensive
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
--- app/src/main/java/com/example/mamunzahir/designs/MainActivity.java
+++ app/src/main/java/com/example/mamunzahir/designs/MainActivity.java
@@ -8,32 +8,59 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.example.mamunzahir.designs.R.id.wind;
+
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
     private Resources mResources;
     private TextView mTextView;
+    private TextView temp;
+    private TextView wind;
+    String imageUrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myfinal);
+        temp= (TextView) findViewById(R.id.textView);
+        wind= (TextView) findViewById(R.id.textView5);
 
-        // Get the application context
-        mContext = getApplicationContext();
+        imageUrl = "http://api.openweathermap.org/data/2.5/weather?q="+"Dhaka"+"&units=metric&APPID=b683a7fcfe33979bf18003ebc9c397dd";
+        JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, imageUrl, (String)null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
-        // Get the Resources
-        mResources = getResources();
-       // mTextView = (TextView) findViewById(R.id.tv);
-       // applyBlurMaskFilter(mTextView, BlurMaskFilter.Blur.OUTER);
+                try {
+                    JSONObject obj=response.getJSONObject("main");
+                    JSONObject obj2=response.getJSONObject("wind");
+                    String temperature=obj.getString("temp");
+                    String speed=obj2.getString("speed");
+                    temp.setText(temperature);
+                    wind.setText("Wind : "+speed);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        AppController.getInstance().addToRequestQueue(request);
+
     }
-    protected void applyBlurMaskFilter(TextView tv, BlurMaskFilter.Blur style){
 
-        float radius = tv.getTextSize()/8;
-
-        // Initialize a new BlurMaskFilter instance
-        BlurMaskFilter filter = new BlurMaskFilter(radius,style);
-
-        tv.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-
-        tv.getPaint().setMaskFilter(filter);
-    }
 }
+
